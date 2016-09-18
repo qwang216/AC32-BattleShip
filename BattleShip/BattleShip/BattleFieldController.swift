333d5f47f917a4e2774a0db46d58a1a001f817f3
Jason Wang <qwang216@gmail.com>
2016-09-18 17:08:48 -0400

    programatic UI

---
+++ BattleShip/BattleShip/BattleFieldController.swift
@@ -0,0 +1,19 @@
+//
+//  BattleFieldController.swift
+//  BattleShip
+//
+//  Created by Jason Wang on 9/18/16.
+//  Copyright © 2016 Jason Wang. All rights reserved.
+//
+
+import UIKit
+
+class BattleFieldController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .whiteColor()
+        navigationItem.title = "Battle Ship"
+    }
+
+}
